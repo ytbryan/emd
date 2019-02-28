899fc507dcdd821df81dac4f34ed6fce87a56d03
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2019-02-28 23:32:17 +0100

    Extract Markdown Renderer creation.

--- config/initializers/markdown_template_handler.rb
+++ config/initializers/markdown_template_handler.rb
@@ -3,6 +3,10 @@ module MarkdownTemplateHandler
     @erb ||= ActionView::Template.registered_template_handler(:erb)
   end
 
+  def self.markdown
+    @markdown ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML, options)
+  end
+
   def self.call(template)
     options = {
       space_after_headers:          true,
@@ -15,9 +19,8 @@ module MarkdownTemplateHandler
       no_intra_emphasis:            true,
       autolink:                     true
     }
-    @markdown ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML, options)
     compiled_source = erb.call(template)
-    "#{@markdown}.render(begin;#{compiled_source};end).html_safe"
+    "#{markdown}.render(begin;#{compiled_source};end).html_safe"
   end
 end
 
